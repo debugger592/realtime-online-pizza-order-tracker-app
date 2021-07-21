bc4e061353d1d2852ae21b8a32533c60f8bceaa2
Amit Kumar <79701922+AmitKumar-07@users.noreply.github.com>
2021-07-21 11:03:29 -0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -50,29 +50,6 @@ let order = hiddenInput ? hiddenInput.value : null
 order = JSON.parse(order)
 let time = document.createElement('small')
 
-function updateStatus(order) {
-    statuses.forEach((status) => {
-        status.classList.remove('step-completed')
-        status.classList.remove('current')
-    })
-    let stepCompleted = true;
-    statuses.forEach((status) => {
-       let dataProp = status.dataset.status
-       if(stepCompleted) {
-            status.classList.add('step-completed')
-       }
-       if(dataProp === order.status) {
-            stepCompleted = false
-            time.innerText = moment(order.updatedAt).format('hh:mm A')
-            status.appendChild(time)
-           if(status.nextElementSibling) {
-            status.nextElementSibling.classList.add('current')
-           }
-       }
-    })
-
-}
-
-updateStatus(order);
+f
 
 
